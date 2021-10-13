143dbba2349b598f5677be991713613599d65cc5
danielacarv <d.c.carvajal29@gmail.com>
2021-10-12 21:53:10 -0500

    Archivos para practica 3

---
+++ Assets/scripts/touch.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class touch : MonoBehaviour
+{
+    public Texture[] texture;
+    public int currentTexture;
+
+    void Start()
+    {
+        Screen.orientation = ScreenOrientation.Landscape;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touches[0].phase == TouchPhase.Began)
+        {
+            currentTexture++;
+            currentTexture %= texture.Length;
+            gameObject.GetComponent<Renderer>().material.mainTexture = texture[currentTexture];
+        }
+    }
+}
